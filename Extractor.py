666e68c401e23d02f1ddd214834a771d5f9e9dd3
IAmBgWithCid <devkavyasmrgamer@gmail.com>
2026-03-16 15:43:59 +0530

    Add comments to Extractor.py and remove epub from tracking

--- Extractor.py
+++ Extractor.py
@@ -1,20 +1,23 @@
 from bs4 import BeautifulSoup
 from ebooklib import epub
 from ebooklib import ITEM_DOCUMENT
-
+#Add the books path here.
 path = "The Way of Kings_ The Stormlight Archive.epub"
-
+#read the ebook.
 book = epub.read_epub(path)
-
+#creating a dictionary to store all the chapters.
 chapters = {}
-
+#iterating through the book.
 for item in book.get_items_of_type(ITEM_DOCUMENT):
+    #filtering out acknolegements and endnotes etc.
     if any(keyword in item.get_name() for keyword in ["chapter", "prologue", "epilogue", "part"]):
+        #removing html tags
         soap = BeautifulSoup(item.get_body_content(), 'html.parser')
+        #storing chapter to dictionary
         chapters[item.get_name()] = soap.get_text()
 
 count = 0
-
+# just to verify we go the chapter
 for key in chapters.keys():
     print(chapters[key])
     count += 1
